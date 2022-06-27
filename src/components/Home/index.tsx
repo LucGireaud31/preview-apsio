f0d211b6ce6be23c6c38467b6b2fa7ab002491bb
Luc <l.gireaud@nomattitude.net>
2022-06-27 10:46:03 +0200

    Refactor Plane component

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,56 +1,63 @@
 import { useEffect, useRef, useState } from "react";
-import { BufferGeometry, DirectionalLight, DoubleSide, FlatShading, Material, Mesh, MeshPhongMaterial, PlaneGeometry } from "three"
-import { Canvas, useFrame, useThree } from '@react-three/fiber'
+import {
+  BufferGeometry,
+  DirectionalLight,
+  DoubleSide,
+  FlatShading,
+  Material,
+  Mesh,
+  MeshPhongMaterial,
+  PlaneGeometry,
+} from "three";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
 
-interface HomeProps {
-};
+interface HomeProps {}
 
 export function Home(props: HomeProps) {
-    const { } = props;
+  const {} = props;
 
-    return (
-        <Canvas style={{ width: innerWidth, height: innerHeight }}>
-            <Plane />
-        </Canvas>
-    );
+  return (
+    <Canvas style={{ width: innerWidth, height: innerHeight }}>
+      <Plane />
+    </Canvas>
+  );
 }
 
 function Plane() {
+  // Constantes
+  const three = useThree();
+  const plane = new PlaneGeometry(3, 3, 10, 10);
+  const material = new MeshPhongMaterial({
+    color: "orange",
+    side: DoubleSide,
+    flatShading: true,
+  });
+  const mesh = new Mesh(plane, material);
 
-    const three = useThree();
+  // Each frames
+  useFrame((state, delta) => {
+    mesh.rotation.y += 0.01;
+  });
 
-
-
-    const plane = new PlaneGeometry(5, 5, 10, 10)
-
-    const material = new MeshPhongMaterial({
-        color: 'orange',
-        side: DoubleSide,
-        flatShading: true
-    })
-    const mesh = new Mesh(plane, material)
-  
-
-    const positions = mesh.geometry.attributes.position.array ?? []
+  useEffect(() => {
+    const positions = (mesh.geometry.attributes.position.array ??
+      []) as number[];
 
     for (let i = 0; i < positions.length; i += 3) {
-        const x = positions[i]
-        const y = positions[i + 1]
-        const z = positions[i + 2]
+      const x = positions[i];
+      const y = positions[i + 1];
+      const z = positions[i + 2];
 
-        positions[i + 2] = z + Math.random()
+      positions[i + 2] = z + Math.random();
     }
 
-    useFrame((state, delta) => {
-        mesh.rotation.y += 0.01
-    })
+    const light = new DirectionalLight();
+    light.position.set(0, 0, 1);
 
-    const light = new DirectionalLight()
-    light.position.set(0,0,1)
+    three.scene.clear();
+    three.scene.add(mesh);
+    three.scene.add(light);
+  }, [mesh]);
 
-    three.scene.clear()
-    three.scene.add(mesh)
-    three.scene.add(light)
-
-    return <></>
+  return <></>;
 }
