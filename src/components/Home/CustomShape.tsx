9fd533cfb556b44a27965d59ebc5313676bdd0cd
Luc <l.gireaud@nomattitude.net>
2022-06-28 13:33:17 +0200

    fix

--- src/components/Home/CustomShape.tsx
+++ src/components/Home/CustomShape.tsx
@@ -1,57 +1,48 @@
-import { useThree } from "@react-three/fiber";
-import {MaterialLoader, ObjectLoader, PointLight } from "three";
-import React, { useRef } from 'react'
-import { useGLTF } from '@react-three/drei'
+import { useLoader, useThree } from "@react-three/fiber";
+import { MaterialLoader, ObjectLoader, PointLight } from "three";
+import { MTLLoader } from "three/examples/jsm/loaders/MTLLoader";
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
 
- export function Model({ ...props }) {
-    const group = useRef(null)
-    const { nodes, materials } = useGLTF('customShape/shape.glb')
-    return (
-      <group ref={group} {...props} dispose={null} scale={1}>
-        <mesh geometry={nodes.Box_1.geometry} material={materials['Cupcake 1']} position={[0.08, 0, 0]} rotation={[-Math.PI / 2, 0, 0]} scale={0} />
-        <mesh geometry={nodes.Box_1_1.geometry} material={materials['Cupcake 1']} position={[0.04, 0, -0.07]} rotation={[-Math.PI / 2, 0, Math.PI / 3]} scale={0} />
-        <mesh geometry={nodes.Box_1_2.geometry} material={materials['Cupcake 1']} position={[-0.04, 0, -0.07]} rotation={[-Math.PI / 2, 0, 2.09]} scale={0} />
-        <mesh geometry={nodes.Box_1_3.geometry} material={materials['Cupcake 1']} position={[-0.08, 0, 0]} rotation={[-Math.PI / 2, 0, Math.PI]} scale={0} />
-        <mesh geometry={nodes.Box_1_4.geometry} material={materials['Cupcake 1']} position={[-0.04, 0, 0.07]} rotation={[-Math.PI / 2, 0, -2.09]} scale={0} />
-        <mesh geometry={nodes.Box_1_5.geometry} material={materials['Cupcake 1']} position={[0.04, 0, 0.07]} rotation={[-Math.PI / 2, 0, -Math.PI / 3]} scale={0} />
-        <mesh geometry={nodes.Polyhedron.geometry} material={materials['Amelia 2']} rotation={[-Math.PI / 2, 0, 0]} scale={0} />
-        <mesh geometry={nodes.Plane.geometry} material={materials.Tuscan} rotation={[-Math.PI / 2, 0, 0]} scale={0} />
-      </group>
-    )
-  }
+//  export function Model({ ...props }) {
+//     const group = useRef(null)
 
+//     const { nodes, materials } = useGLTF('customShape/shape.glb')
+//     return (
+//       <group ref={group} {...props} dispose={null} scale={1}>
+//         <mesh geometry={nodes.Box_1.geometry} material={materials['Cupcake 1']} position={[0.08, 0, 0]} rotation={[-Math.PI / 2, 0, 0]} scale={0} />
+//         <mesh geometry={nodes.Box_1_1.geometry} material={materials['Cupcake 1']} position={[0.04, 0, -0.07]} rotation={[-Math.PI / 2, 0, Math.PI / 3]} scale={0} />
+//         <mesh geometry={nodes.Box_1_2.geometry} material={materials['Cupcake 1']} position={[-0.04, 0, -0.07]} rotation={[-Math.PI / 2, 0, 2.09]} scale={0} />
+//         <mesh geometry={nodes.Box_1_3.geometry} material={materials['Cupcake 1']} position={[-0.08, 0, 0]} rotation={[-Math.PI / 2, 0, Math.PI]} scale={0} />
+//         <mesh geometry={nodes.Box_1_4.geometry} material={materials['Cupcake 1']} position={[-0.04, 0, 0.07]} rotation={[-Math.PI / 2, 0, -2.09]} scale={0} />
+//         <mesh geometry={nodes.Box_1_5.geometry} material={materials['Cupcake 1']} position={[0.04, 0, 0.07]} rotation={[-Math.PI / 2, 0, -Math.PI / 3]} scale={0} />
+//         <mesh geometry={nodes.Polyhedron.geometry} material={materials['Amelia 2']} rotation={[-Math.PI / 2, 0, 0]} scale={0} />
+//         <mesh geometry={nodes.Plane.geometry} material={materials.Tuscan} rotation={[-Math.PI / 2, 0, 0]} scale={0} />
+//       </group>
+//     )
+//   }
 
 export function CustomShape() {
+  const { setSize, scene, camera } = useThree();
 
+  addEventListener("resize", () => {
+    setSize(innerWidth, innerHeight);
+  });
 
-const {setSize,scene} = useThree();
+  const light = new PointLight(0xff0000, 1.4, 1000);
+  light.position.set(0, 15, 15);
 
-addEventListener("resize",()=>{
-    setSize(innerWidth,innerHeight)
-    
-})
+  scene.clear();
+  scene.add(light);
 
-const light = new PointLight(0xFFFFFF,1.4,1000);
-light.position.set(0,15,15)
+  const mtl = useLoader(MTLLoader, "customShape/center.mtl");
 
-scene.clear()
-scene.add(light)
+  const obj = useLoader(OBJLoader, "customShape/center.obj", (l: any) => {
+    mtl.preload();
+    l.setMaterials(mtl);
+  });
+  camera.position.z = 200;
 
-// const mtl = new MaterialLoader();
-// mtl.load("https://s3-us-west-2.amazonaws.com/s.cdpn.io/2621168/glass.mtl",(materials)=>{
-//     {console.log("salut")}
-//     materials.preload();
+  scene.add(obj);
 
-//     const obj = new ObjectLoader();
-//     obj.setMaterials(materials)
-//     obj.load("https://s3-us-west-2.amazonaws.com/s.cdpn.io/2621168/glass.obj",(obj)=>{
-//         scene.add(obj)
-//     })
-
-
-// })
-
-return (
-<></>
-);
+  return <></>;
 }
--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,29 +1,22 @@
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
-import { CustomShape, Model } from "./CustomShape";
+import { CustomShape } from "./CustomShape";
 import { MontainsMap } from "./MontainsMap";
 
 export interface ICoord {
-  x: number,
-  y: number
+  x: number;
+  y: number;
 }
 
-interface HomeProps { }
+interface HomeProps {}
 
 export function Home(props: HomeProps) {
-  const { } = props;
+  const {} = props;
 
   return (
-    <Canvas style={{ width: innerWidth, height: innerHeight }} >
+    <Canvas style={{ width: innerWidth, height: innerHeight }}>
       {/* <MontainsMap /> */}
-      <Suspense fallback={false}>
-
-      <Model/>
-      <ambientLight/>
-      </Suspense>
+      <CustomShape />
     </Canvas>
   );
 }
-
-
-
