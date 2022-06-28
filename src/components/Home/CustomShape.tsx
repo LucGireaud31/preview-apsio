5a86fd4e55cfacf357fa704e3418682ea03e6078
luc.gireaud@gmail.com <luc.gireaud@gmail.com>
2022-06-28 19:17:23 +0200

    Animation on custom component

--- src/components/Home/CustomShape.tsx
+++ src/components/Home/CustomShape.tsx
@@ -1,8 +1,9 @@
-import { useLoader, useThree } from "@react-three/fiber";
-import { AmbientLight } from "three";
+import { useFrame, useLoader, useThree } from "@react-three/fiber";
+import { AmbientLight, DirectionalLight, PointLight } from "three";
 import { MTLLoader } from "three/examples/jsm/loaders/MTLLoader";
 import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import gsap from "gsap";
 
 export function CustomShape() {
   const { setSize, scene, camera, gl } = useThree();
@@ -11,25 +12,49 @@ export function CustomShape() {
     setSize(innerWidth, innerHeight);
   });
 
-  const light = new AmbientLight();
-  light.position.set(0, 0, 1);
-
+  const light = new PointLight();
+  light.position.set(0, 150, 150);
+  
   scene.clear();
   scene.add(light);
-
+  
   // Custom obj
-  const mtl = useLoader(MTLLoader, "customShape/center.mtl");
-  const obj = useLoader(OBJLoader, "customShape/center.obj", (l: any) => {
+  const mtl = useLoader(MTLLoader, "customShape/around.mtl");
+  const obj = useLoader(OBJLoader, "customShape/around.obj",
+   (l: any) => {
     mtl.preload();
     l.setMaterials(mtl);
   });
+  const mtl2 = useLoader(MTLLoader, "customShape/center.mtl");
+  const obj2 = useLoader(OBJLoader, "customShape/center.obj",
+   (l: any) => {
+    mtl2.preload();
+    l.setMaterials(mtl2);
 
-  camera.position.z = 200;
+    obj.position.y = 500
+
+    gsap.to(obj.position,{
+        y:0,
+        duration:2.5,
+        ease:"elastic.out(0.5, 0.3)"
+    })
+  });
+
+  camera.position.z = 500;
+  camera.position.y = 200;
+
+
 
   // orbital control
   const o = new OrbitControls(camera, gl.domElement);
-
+  
+  useFrame(()=>{
+    obj.rotation.y+=0.01
+    obj2.rotation.y-=0.01
+  })
+  
   scene.add(obj);
+  scene.add(obj2);
 
   return <></>;
 }
