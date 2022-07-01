71ea06298b370f12e42ebfae1a37cf1e774c7e0c
luc.gireaud@gmail.com <luc.gireaud@gmail.com>
2022-07-01 20:34:50 +0200

    auto mouvement on objects

--- src/components/Home/MouseControl.tsx
+++ src/components/Home/MouseControl.tsx
@@ -1,21 +1,23 @@
 import { useFrame, useLoader, useThree } from "@react-three/fiber"
-import { Group, PointLight } from "three";
+import { Group, Object3D, PointLight, Vector3 } from "three";
 import { MTLLoader } from "three/examples/jsm/loaders/MTLLoader";
 import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
 import gsap from "gsap";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { useEffect, useState } from "react";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 
 const nbElements = 9;
 
 export function MouseControl() {
 
-    const { gl, scene, camera,size } = useThree()
+    const { gl, scene, camera, size } = useThree()
 
     const [coord, setCoord] = useState([0, 0])
 
     function generateObject(index: number) {
+
         const mtl = useLoader(MTLLoader, `customShape/center - Copie (${index}).mtl`);
         const obj = useLoader(OBJLoader, `customShape/center - Copie (${index}).obj`,
             (l: any) => {
@@ -33,6 +35,10 @@ export function MouseControl() {
         obj.position.y = coord[1] + (index * 50)
 
         obj.name = `obj_${index}`
+        obj.userData = {
+            delta: 0
+        }
+
         scene.add(obj);
     }
 
@@ -46,29 +52,38 @@ export function MouseControl() {
 
 
     // Custom obj
+
     for (let i = 0; i < nbElements; i++) {
         generateObject(i)
     }
-
     useFrame((state) => {
         for (let i = 0; i < nbElements; i++) {
             const obj = state.scene.getObjectByName(`obj_${i}`)
             if (obj) {
 
-                obj.position.x += 1
+                const delta = obj.userData.delta;
+
+                const newX = Math.cos(delta) * 1
+                const newZ = Math.sin(delta) * 1
+
+                obj.translateX(newX)
+                obj.translateZ(newZ)
+                obj.rotateY(0.01)
+
+
+                obj.userData.delta += 0.08
             }
         }
     })
 
     useEffect(() => {
-        camera.position.z = 500;
-
+        camera.position.z = 800;
 
         gl.domElement.addEventListener("mousemove", (e) => {
             const x = e.clientX
             const y = e.clientY
-
-            setCoord([(x * 2 / gl.domElement.width - 1) * size.width*0.85/2, (y * 2 / gl.domElement.height - 1) * -(size.height*0.85/2) - 50])
+           
+            setCoord([(x * 2 / gl.domElement.width - 1) * size.width * 1.30 / 2, (y * 2 / gl.domElement.height - 1) * -(size.height * 1.3 / 2) - 50])
         })
     }, [])
 
